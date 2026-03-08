efbe744722abdd7dc760277a7908dcac017f2f9f
Hana Guwanto <hanaguwanto@gmail.com>
2026-03-09 06:22:50 +0700

    Add GET /todos endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, jsonify
+from database import get_connection
 
 app = Flask(__name__)
 
 @app.route("/")
 def health_check():
-    return {"status": "ok"}
+    conn = get_connection()
+    conn.close()
+    return {"status": "backend + db ok"}
+
+@app.route("/todos", methods=["GET"])
+def get_todos():
+    conn = get_connection()
+    cursor = conn.cursor(dictionary=True)
+
+    cursor.execute("SELECT * FROM todos")
+    todos = cursor.fetchall()
+    
+    cursor.close()
+    conn.close()
+    
+    return jsonify(todos)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+    
